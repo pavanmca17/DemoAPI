68e62619523d743706e3f8b4f86ea6dda0372b56
pavanmca17 <33794943+pavanmca17@users.noreply.github.com>
2019-04-12 14:45:42 +0530

    Made required changes

--- DemoAPI/MiddleWare/CorealtionIDMiddleWare.cs
+++ DemoAPI/MiddleWare/CorealtionIDMiddleWare.cs
@@ -1,1 +1,43 @@
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
 
+namespace DemoAPI.MiddleWare
+{
+    public class CorealtionIDMiddleWare
+    {
+        private readonly RequestDelegate _next;
+
+        public CorealtionIDMiddleWare(RequestDelegate requestDelegate)
+        {
+            _next = requestDelegate;
+        }
+
+        //constructor and service injection
+        public async Task Invoke(HttpContext httpContext)
+        {
+           await LogConfigurationID(httpContext);
+           await _next(httpContext);
+        }
+
+        private static Task LogConfigurationID(HttpContext context)
+        {
+             if (context.Request.Headers.TryGetValue("CorealtionID", out StringValues correlationId))
+             {    context.TraceIdentifier = correlationId;
+             }
+             
+            // apply the correlation ID to the response header for client side tracking
+            context.Response.OnStarting(() =>
+            {
+                   context.Response.Headers.Add(_options.Header, new[] { context.TraceIdentifier });
+                   return Task.CompletedTask;
+            });
+
+            return _next(context);
+       }
+
+    }
+}
